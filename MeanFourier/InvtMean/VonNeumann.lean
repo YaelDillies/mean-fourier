e4286a6a2af1208db1db788fe4cbc2ed0acf3196
Yaël Dillies <yael.dillies@gmail.com>
2026-06-22 14:32:44 +0200

    The von Neumann mean

---
+++ MeanFourier/InvtMean/VonNeumann.lean
@@ -0,0 +1,59 @@
+/-
+Copyright (c) 2026 Yaël Dillies. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Yaël Dillies
+-/
+module
+
+public import MeanFourier.AlmostPeriod.Uniform
+public import MeanFourier.InvtMean.Defs
+
+/-!
+# The von Neumann mean
+
+This file defines the von Neumann mean.
+
+## References
+
+* [*Almost periodic functions in a group. I*, John von Neumann](https://doi.org/10.2307/1989792)
+-/
+
+public section
+
+open Bornology
+
+namespace InvtMean
+variable {G : Type*} [Group G]
+
+/-- The von Neumann mean -/
+@[expose, simps]
+noncomputable def vn : InvtMean G where
+  IsMeasFun f := IsUAP f
+  toFun f :=
+    open scoped Classical in
+    if hf : IsUAP f then
+      hf.isAlmostConvergent.existsUnique_const_mem_closure_convexHull.choose
+    else
+      0
+  map_zero := by simp; grind
+  map_add := by
+    rintro f hf g hg
+    rw [dif_pos hf, dif_pos hg, dif_pos (hf.add hg),
+      (hf.add hg).isAlmostConvergent.existsUnique_const_mem_closure_convexHull.choose_eq_iff]
+    sorry
+  map_smul := by
+    rintro f hf z
+    rw [dif_pos hf, dif_pos hf.smul,
+      hf.smul.isAlmostConvergent.existsUnique_const_mem_closure_convexHull.choose_eq_iff]
+    sorry
+  map_translate := by
+    rintro f hf x
+    rw [dif_pos hf, dif_pos hf.translate,
+      hf.translate.isAlmostConvergent.existsUnique_const_mem_closure_convexHull.choose_eq_iff]
+    sorry
+  map_nonneg := by
+    rintro f hf₀ hf
+    rw [dif_pos hf]
+    sorry
+
+end InvtMean
